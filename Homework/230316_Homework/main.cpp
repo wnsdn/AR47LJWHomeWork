5ba36f77d8a7a73c9359735391aa5f3f7b4b7061
June <pllwnsdn@naver.com>
2023-03-16 20:42:52 +0900

    이준우 : 230316 가변인자 접근 숙제 업로드

--- Homework/230316_Homework/main.cpp
+++ Homework/230316_Homework/main.cpp
@@ -1,8 +1,23 @@
 ﻿#include <iostream>
 
+void VariableArgumentFunction(int _reps, ...)
+{
+	int reps = _reps;
+	int* ptr = &_reps;
+	int Value = 0;
+	int i = 1;
+
+	while (reps--)
+	{
+		Value = ptr[i * 2];
+		++i;
+	}
+}
+
 int main()
 {
-
+	VariableArgumentFunction(3, 10, 20, 30);
+	VariableArgumentFunction(2, -20, 74);
 
 	return 0;
 }
